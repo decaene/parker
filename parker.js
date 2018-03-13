a5faaa55178bdefe95bae358f2cc7f7714b05fed
Codigeek Apps <pbarrera@codigeek.com>
2018-03-13 11:52:24 -0600

    first commit

--- parker.js
+++ parker.js
@@ -286,6 +286,26 @@ router.post("/get_usuarios_empresa",function(req,res){
     });
 });
 
+router.post("/get_ciudades",function(req,res){
+    var collection    =  datb.collection('Ciudad');
+    collection.aggregate([
+    ]).toArray(function(err, result){  
+        if(err){
+            var res_err      = {};
+            res_err.status   = "error";
+            res_err.error    = err;
+            res_err.message  = err;
+            res.send(res_err);
+        }else{
+            var res_data      = {};
+            res_data.status   = "success";
+            res_data.message  = "Ciudades";
+            res_data.data     = result;
+            res.send(res_data);
+        }
+    });
+});
+
 router.post("/get_vehiculos_empresa",function(req,res){
     var collection    =  datb.collection('Vehiculo');
     collection.aggregate([
