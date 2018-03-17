b823411296df256937fb1762980fe30c829ae63f
Codigeek Apps <pbarrera@codigeek.com>
2018-03-17 17:44:10 -0600

    first commit

--- parker.js
+++ parker.js
@@ -107,6 +107,17 @@ io.on('connection', (socket) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
+var readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
 
 function enviar_correo(correo, usuario, mensaje, solicitud){
 	
