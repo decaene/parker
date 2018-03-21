8b20b0e59fe2937ffb68d5e825d85d38339a064c
Codigeek Apps <pbarrera@codigeek.com>
2018-03-21 17:52:03 -0600

    first commit

--- parker.js
+++ parker.js
@@ -529,6 +529,32 @@ router.post("/get_usuarios_despacho",function(req,res){
     });
 });
 
+router.post("/get_usuarios_repartidores",function(req,res){
+    var collection    =  datb.collection('Usuario');
+    collection.aggregate([
+		{ $match:  { "almacen_id" : ObjectId(req.body.almacen._id) , "tipo_usuario_id" : ObjectId("5ab2edb68fd9b9c63485baa3") } },
+		{ $lookup: { from: "Tipo_Usuario", localField: "tipo_usuario_id", foreignField: "_id", as: "tipo_usuario" } },
+		{ $lookup: { from: "Banco", localField: "banco_id", foreignField: "_id", as: "banco" } },
+		{ $lookup: { from: "Tipo_Empleado", localField: "tipo_empleado_id", foreignField: "_id", as: "tipo_empleado" } },
+		{ $lookup: { from: "Ciudad", localField: "ciudad_id", foreignField: "_id", as: "ciudad" } },
+		{ $lookup: { from: "Tipo_Comision", localField: "tipo_comision_id", foreignField: "_id", as: "tipo_comision" } }
+    ]).toArray(function(err, result){  
+        if(err){
+            var res_err      = {};
+            res_err.status   = "error";
+            res_err.error    = err;
+            res_err.message  = err;
+            res.send(res_err);
+        }else{
+            var res_data      = {};
+            res_data.status   = "success";
+            res_data.message  = "Usuarios";
+            res_data.data     = result;
+            res.send(res_data);
+        }
+    });
+});
+
 router.post("/get_usuarios_empleados",function(req,res){
     var collection    =  datb.collection('Usuario');
     collection.aggregate([
@@ -1014,6 +1040,16 @@ router.post("/nuevo_usuario_empresa",function(req,res){
 			console.log("Cliente - Nuevo Usuario");
 			req.body.data.ciudad_id	= new ObjectId(req.body.data.ciudad._id);
 			delete req.body.data.banco;
+			delete req.body.data.tipo_empleado;
+			delete req.body.data.tipo_comision;
+			delete req.body.data.ciudad;
+			delete req.body.data.tipo_usuario;
+		break;
+		case '5ab2edb68fd9b9c63485baa3':
+			console.log("Repartidor - Nuevo Usuario");
+			req.body.data.almacen_id = new ObjectId(req.body.data.almacen._id);
+			delete req.body.data.banco;
+			delete req.body.data.almacen;
 			delete req.body.data.tipo_empleado;
 			delete req.body.data.tipo_comision;
 			delete req.body.data.ciudad;
