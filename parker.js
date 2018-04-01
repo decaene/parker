caaff338778feff14c48231b359a39e61ea2b798
Codigeek Apps <pbarrera@codigeek.com>
2018-03-31 19:43:51 -0600

    first commit

--- parker.js
+++ parker.js
@@ -1453,7 +1453,7 @@ router.post("/update_foto_usuario",function(req,res){
 			else{
 				var res_err      = {};
 				res_err.status   = "success";
-				res_err.foto   	 = 'usuario/'+req.body.data._id+'_foto.png'
+				res_err.foto   	 = 'http://165.227.30.166:3017/usuario/'+req.body.data._id+'_foto.png'
 				res_err.message  = "Actualizaste tu foto";
 				res_err.result	 = result;
 				res.send(res_err);
