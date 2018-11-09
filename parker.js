8cd45f6d6fed8cae96d1440feb1756605ee1f5b8
Codigeek Apps <pbarrera@codigeek.com>
2018-11-08 18:31:10 -0600

    first commit

--- parker.js
+++ parker.js
@@ -2549,7 +2549,7 @@ router.post("/actualizar_venta_comprobante_cliente",function(req,res){
 							enviar_correo_despacho(req.body.venta.despacho.correo , req.body.venta);
 							enviar_correo(req.body.venta.repartidor.email, req.body.venta.repartidor.nombre, "Folio completado", "El repartidor terminó el proceso.");
 						
-							console.log("Correo a repartidor desde captura cliente");
+							console.log("Correo a repartidor desde captura cliente"); 
 							console.log(req.body.venta.repartidor.email);
 							console.log(req.body.venta.repartidor.nombre);
 						
